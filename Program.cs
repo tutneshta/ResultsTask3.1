66ac8256935ac8e7e0d9cfc1b1aed34cd4a11cb1
tutneshta <tutneshta@yandex.ru>
2022-09-06 14:07:04 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ResultsTask3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\Task1\";
+
+            long temp1 = XFile.GetSizeDir(path);
+
+            XFile.DeliteFile(path);
+
+            Console.WriteLine($"Исходный размер папки: {temp1} байт \nОсвобождено: {XFile.size} байт \nФайлов удалено: {XFile.countFile} \nТекущий размер папки: {temp1 - XFile.size} байт");
+        }
+    }
+}
---
+++ XFile.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ResultsTask3._1
+{
+    public static class XFile
+    {
+        public static long count = 0;
+             
+        public static long size = 0;
+
+        public static int countFile = 0;
+
+        public static long GetSizeDir(string path)
+        {
+
+            DirectoryInfo fileAndDirectory = new DirectoryInfo(path);
+
+            if (!fileAndDirectory.Exists)
+            {
+                Console.WriteLine("папка по заданному адресу не существует");
+                return 0;
+            }
+            else
+            {
+                try
+                {
+                    foreach (FileInfo f in fileAndDirectory.GetFiles())
+                    {
+
+                        XFile.count = (long)(count + f.Length);
+                    }
+
+                    foreach (DirectoryInfo d in fileAndDirectory.GetDirectories())
+                    {
+                        GetSizeDir(d.FullName);
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ошибка: ", ex.Message);
+                }
+            }
+
+            return count;
+
+        }
+        public static void DeliteFile(string path)
+        {
+
+            DirectoryInfo fileAndDirectory = new DirectoryInfo(path);
+
+            var delay = 1;
+
+            if (!fileAndDirectory.Exists)
+            {
+                Console.WriteLine("папка по заданному адресу не существует");
+            }
+            else
+            {
+
+                foreach (FileInfo f in fileAndDirectory.GetFiles())
+                {
+
+                    if (f.LastWriteTime < DateTime.Now.AddMinutes(-delay))
+                    {
+
+                        try
+                        {
+
+                            XFile.size = size + f.Length;
+
+                            f.Delete();
+
+                            countFile++;
+                        }
+                        catch (UnauthorizedAccessException e)
+                        {
+                            Console.WriteLine("Отсутствует доступ к файлу. Ошибка: " + e.Message);
+                        }
+                    }
+
+                }
+
+                foreach (DirectoryInfo d in fileAndDirectory.GetDirectories())
+                {
+
+                    if (d.LastWriteTime < DateTime.Now.AddMinutes(-delay))
+                    {
+                        DeliteFile(d.FullName);
+
+                        try
+                        {
+                            d.Delete();
+                        }
+
+                        catch (UnauthorizedAccessException e)
+                        {
+                            Console.WriteLine("Отсутствует доступ к папке. Ошибка: " + e.Message);
+                        }
+
+                    }
+                }
+
+            }
+
+        }
+
+    }
+}
